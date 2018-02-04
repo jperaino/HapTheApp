dc5461ee165cf2dc1d855519b5ba885b886788df
jperaino <jprn72@gmail.com>
2018-02-03 19:44:39 -0500

    Initial Commit

---
+++ HapTheApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HapTheApp
+//
+//  Created by Jim Peraino on 2/3/18.
+//  Copyright © 2018 James Peraino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

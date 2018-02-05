6a6d118014350f7e12aa3aa43a76d14c25aad0b5
jperaino <jprn72@gmail.com>
2018-02-05 11:07:44 -0500

    feat: setup search bar

---
+++ HapTheApp/locationSearchTable.swift
@@ -0,0 +1,21 @@
+//
+//  locationSearchTable.swift
+//  HapTheApp
+//
+//  Created by Jim Peraino on 2/5/18.
+//  Copyright © 2018 James Peraino. All rights reserved.
+//
+
+import UIKit
+class LocationSearchTable : UITableViewController {
+    
+    
+    
+}
+
+// MARK: Extensions
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+}

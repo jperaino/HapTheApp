773f1cb4c8ca2f79197726adda344bc1c75934e4
jperaino <jprn72@gmail.com>
2018-02-11 14:01:04 -0500

    feat: add autocomplete search

--- HapTheApp/gMapsViewController.swift
+++ HapTheApp/gMapsViewController.swift
@@ -78,6 +78,10 @@ class gMapsViewController: UIViewController {
     
     @IBAction func addPlaceBySearch(_ sender: Any) {
         print("adding a place by search")
+        
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
     }
     
     
@@ -157,6 +161,39 @@ extension gMapsViewController: CLLocationManagerDelegate {
     }
 }
 
+
+// Handle GMSAutocompleteViewControllerDelegate
+extension gMapsViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(String(describing: place.formattedAddress))")
+        print("Place attributions: \(String(describing: place.attributions))")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    
+}
 
 
 
